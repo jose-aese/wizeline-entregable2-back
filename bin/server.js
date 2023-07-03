8f1c8a4e8cb5ba3cf9d064e7c6cfd988dd0ce426
JOSE ENRIQUE PEREZ ARELLANO <jose.perezar@bancoazteca.com.mx>
2023-07-03 12:38:24 -0600

    Socket.io

--- bin/server.js
+++ bin/server.js
@@ -1,17 +1,28 @@
 const http = require('http');
 const app = require('../app');
 const mongoose = require("mongoose");
+const {Server} = require("socket.io");
 const port = 3000;
-const server = http.createServer(app);
-
+const portIo = 3001;
+let server = http.Server(app);
 server.listen(port);
 
-mongoose.connect('mongodb://mongo:27017/wazeline')
+
+mongoose.connect('mongodb://localhost:27017/wazeline')
     .then(() => {
         console.log("La conexion a la base de datos se realizo correctamente");
     })
     .catch(err => console.log(err));
 
+const io = new Server(portIo);
+
+io.on("connection", (socket) => {
+    socket.on("howdy", (arg) => {
+        console.log(arg)
+        socket.emit('new message', `Bienvenido ${arg.name}`);
+    })
+
+})
 
 server.on('listening', () => {
     console.log(`El server esta en el puerto ${port}`)
